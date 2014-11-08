c6c3a7e8c4892f3884cce97f58ff10d3fe5e8168
Erik Suta <erik.suta@evolveum.com>
2014-11-08 15:36:52 +0100

    Update

---
+++ dbpedia/src/test/java/com/eriksuta/parser/ParserTest.java
@@ -0,0 +1,62 @@
+package com.eriksuta.parser;
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openrdf.model.Model;
+import org.openrdf.model.impl.LinkedHashModel;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.StatementCollector;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  @author shood
+ * */
+public class ParserTest {
+
+    private static final String DIR = "src/test/resources/rdf/";
+
+    private static final String F_CATEGORY_LABELS_SK = DIR + "category_labels_sk.ttl";
+
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("==========|BASIC PARSER TEST SUIT START|==========");
+
+    }
+
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("==========|BASIC PARSER TEST SUIT END|==========");
+    }
+
+    @Test
+    public void test_01_parse_category_labels_sk(){
+        File file = new File(F_CATEGORY_LABELS_SK);
+
+        RDFParser parser = Rio.createParser(RDFFormat.TURTLE);
+        List myGraph = new ArrayList();
+        parser.setRDFHandler(new StatementCollector(myGraph));
+
+        try{
+            FileInputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis);
+
+            parser.parse(isr, "");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        System.out.println(myGraph.size());
+    }
+
+
+
+
+}
